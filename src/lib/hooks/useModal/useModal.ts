4b64856a3560a1a0d868ecfcc07a3f52dd4941ab
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-02-25 22:45:00 +0900

    ✨ Feat :  useModal hook

---
+++ src/lib/hooks/useModal/useModal.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from "react";
+
+import { UseModalParams } from "./useModal.type";
+
+const useModal = ({ defaultVisible = false }: UseModalParams = {}) => {
+  const [isOpen, setIsModal] = useState(defaultVisible);
+
+  const openModal = useCallback(() => setIsModal(true), []);
+
+  const closeModal = useCallback(() => setIsModal(false), []);
+
+  return [isOpen, openModal, closeModal] as const;
+};
+
+export default useModal;
