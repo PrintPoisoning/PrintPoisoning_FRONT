efc0096f7a7bb1ea0444bffa410a986f17680160
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-02-26 16:36:54 +0900

    ✨ Feat :  ModalPortal

---
+++ src/lib/components/client/Modal/components/ModalPortal/ModalPortal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
+import { ModalPortalProps } from "./ModalPortal.type";
+
+const ModalPortal = ({ children, isShow }: ModalPortalProps) => {
+  const [ModalBaseElement] = useState<HTMLElement>(document.body);
+
+  useEffect(() => {
+    if (isShow) {
+      const originalStyle = window.getComputedStyle(ModalBaseElement).overflow;
+      document.body.style.overflow = "hidden";
+
+      return () => {
+        document.body.style.overflow = originalStyle;
+      };
+    }
+  }, [isShow, ModalBaseElement]);
+
+  if (!isShow) {
+    return null;
+  }
+
+  return createPortal(<>{children}</>, ModalBaseElement);
+};
+
+export default ModalPortal;
