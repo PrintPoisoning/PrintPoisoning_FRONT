ef2ec7d72f4ff5a22ad7e560b6b1fe6b53c8d8e7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-02-26 16:58:25 +0900

    🎨 Style : color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        transparent_10: "var(--transparent_10)",
+        transparent_30: "var(--transparent_30)",
+        transparent_50: "var(--transparent_50)",
+        transparent_90: "var(--transparent_90)",
+      },
+
+      zIndex: {
+        modal: "500",
       },
     },
   },
