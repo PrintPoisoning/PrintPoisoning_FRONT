e41f8bdf731bde939bb818fabcf0ee6f5286e53b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-02-27 12:25:12 +0900

    ✨ Feat :  Modal Animation 적용

--- src/lib/components/client/Modal/Modal.tsx
+++ src/lib/components/client/Modal/Modal.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { PropsWithChildren, use } from "react";
+import { PropsWithChildren } from "react";
 
 import { CloseIcon } from "@lib/components/server";
-import { useClickAway } from "@lib/hooks";
 
 import { ModalPortal } from "./components";
-import { useClickAwayModal, useKeydownModal } from "./hooks";
+import { useClickAwayModal, useKeydownModal, useModalVariant } from "./hooks";
 
-import { AnimatePresence } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { twMerge } from "tailwind-merge";
 
 type ModalProps = PropsWithChildren<{
@@ -25,31 +24,46 @@ const Modal = ({
   disableAwayClick = false,
   children,
 }: ModalProps) => {
-  const awayClose = useClickAwayModal(onClose);
   useKeydownModal({ isShow, onClose, disableAwayClick });
+  const awayClose = useClickAwayModal(onClose);
+  const { modalVariant, backgroundVariant } = useModalVariant();
 
   return (
     <AnimatePresence>
       {isShow && (
         <ModalPortal isShow={isShow}>
-          <section
+          <motion.section
             className="w-full h-full flex items-center justify-center fixed top-0 left-0 bg-transparent_90 overflow-auto z-modal"
-            onClick={e => !disableAwayClick && awayClose(e)}>
-            <article
+            onClick={e => !disableAwayClick && awayClose(e)}
+            variants={backgroundVariant}
+            initial="close"
+            animate="show"
+            exit="close">
+            <motion.article
               className={twMerge(
                 "min-h-[4rem] min-w-[4rem] p-[1rem] flex justify-end relative bg-white rounded-radius8",
-                !hideCloseIcon && "pt-[2.2rem]",
-              )}>
+                !hideCloseIcon && "pt-[3.2rem]",
+              )}
+              variants={modalVariant}
+              initial="close"
+              animate="show"
+              exit="close">
               {!hideCloseIcon && (
-                <i
+                <motion.i
                   onClick={onClose}
-                  className="w-[1.8rem] h-[1.8rem] absolute top-[0.3rem] cursor-pointer select-none">
+                  className="w-[2rem] h-[2rem] absolute top-[0.6rem] cursor-pointer select-none"
+                  whileTap={{
+                    scale: 1.2,
+                  }}
+                  whileHover={{
+                    opacity: 0.4,
+                  }}>
                   <CloseIcon size="100%" />
-                </i>
+                </motion.i>
               )}
               {children}
-            </article>
-          </section>
+            </motion.article>
+          </motion.section>
         </ModalPortal>
       )}
     </AnimatePresence>
--- src/lib/components/client/Modal/hooks/index.ts
+++ src/lib/components/client/Modal/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useKeydownModal } from "./useKeydownModal/useKeydownModal";
 export { default as useClickAwayModal } from "./useClickAwayModal/useClickAwayModal";
+export { default as useModalVariant } from "./useModalVariant/useModalVariant";
