642bc0f83c06f03893c67eb9ff8ffa26edf26737
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-04 10:23:42 +0900

    ✨ Feat :  Toast 기능 구현

--- src/lib/components/client/Toast/ToastProvider.tsx
+++ src/lib/components/client/Toast/ToastProvider.tsx
@@ -2,11 +2,25 @@
 
 import { PropsWithChildren } from "react";
 
+import { ToastItem } from "./components";
+import { useToastStore } from "./store";
+
 const ToastProvider = ({ children }: PropsWithChildren) => {
+  const { toastList, removeToast } = useToastStore();
   return (
     <>
       {children}
-      <ul className="absolute top-[1rem] right-[1rem] flex flex-col gap-[0.8rem] text-size14 z-toast"></ul>
+      <ul className="absolute top-[1rem] right-[1rem] flex flex-col gap-[0.8rem] text-size14 z-toast">
+        {toastList.map(({ id, message, deleteTime, type }) => (
+          <ToastItem
+            key={id}
+            type={type}
+            message={message}
+            deleteTime={deleteTime}
+            onClose={() => removeToast(id)}
+          />
+        ))}
+      </ul>
     </>
   );
 };
