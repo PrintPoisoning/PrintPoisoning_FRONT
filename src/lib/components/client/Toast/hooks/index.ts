b853f7271999f129ad98861289ef45b404e71da6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-04 10:04:41 +0900

    ✨ Feat :  useToastVariant

--- src/lib/components/client/Toast/hooks/index.ts
+++ src/lib/components/client/Toast/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useToastColor } from "./useToastColor/useToastColor";
 export { default as useToastTimer } from "./useToastTimer/useToastTimer";
+export { default as useToastVariant } from "./useToastVariant/useToastVariant";
---
+++ src/lib/components/client/Toast/hooks/useToastVariant/useToastVariant.ts
@@ -0,0 +1,42 @@
+import { useMemo } from "react";
+
+import { UseToastVariantProps } from "./useToastVariant.type";
+
+const useToastVariant = ({ deleteTime }: UseToastVariantProps) => {
+  const toastLayout = useMemo(
+    () => ({
+      close: {
+        translateX: "100%",
+        scale: 0.5,
+        opacity: 0,
+      },
+      show: {
+        translateX: "0%",
+        scale: 1,
+        opacity: 1,
+      },
+    }),
+    [],
+  );
+
+  const toastProgress = useMemo(
+    () => ({
+      close: { translateX: "-100%" },
+      show: {
+        translateX: "0%",
+      },
+      transition: {
+        duration: deleteTime / 1000 + 0.05,
+        ease: "linear",
+      },
+    }),
+    [deleteTime],
+  );
+
+  return {
+    toastLayout,
+    toastProgress,
+  };
+};
+
+export default useToastVariant;
