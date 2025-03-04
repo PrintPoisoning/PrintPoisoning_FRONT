7ba95549f65c0d07db48a1786289448a4aca3081
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-04 09:53:03 +0900

    ✨ Feat :  useToastTimer

---
+++ src/lib/components/client/Toast/hooks/useToastTimer/useToastTimer.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+import { UseToastTimerProps } from "./useToastTimer.type";
+
+const useToastTimer = ({ time }: UseToastTimerProps) => {
+  const [isShow, setIsShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShow(false);
+    }, time);
+  }, [time]);
+
+  return isShow;
+};
+
+export default useToastTimer;
