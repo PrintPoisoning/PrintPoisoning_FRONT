7f7cdd4dca223d05e435bcf5ddb47482c301f0c7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-04 10:12:30 +0900

    ✨ Feat :  Toast Type 정의

--- src/lib/components/client/Toast/ToastProvider.tsx
+++ src/lib/components/client/Toast/ToastProvider.tsx
@@ -6,7 +6,7 @@ const ToastProvider = ({ children }: PropsWithChildren) => {
   return (
     <>
       {children}
-      <ul className="absolute top-[1rem] left-[1rem] flex flex-col z-toast"></ul>
+      <ul className="absolute top-[1rem] right-[1rem] flex flex-col gap-[0.8rem] text-size14 z-toast"></ul>
     </>
   );
 };
