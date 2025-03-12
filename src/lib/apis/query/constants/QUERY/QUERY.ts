bf3fe267bcef8de6172417eedd70de53edd07d60
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-12 14:53:20 +0900

    ✨ Feat :  getQueryClient 리뉴얼

--- src/lib/apis/query/constants/QUERY/QUERY.ts
+++ src/lib/apis/query/constants/QUERY/QUERY.ts
@@ -1,9 +1,3 @@
-import { cache } from "react";
-
-import { QueryClient } from "@tanstack/react-query";
-
-export const getQueryClient = cache(() => new QueryClient());
-
 export const QUERY_KEY = {
   TEST: ["TEST"],
 };
