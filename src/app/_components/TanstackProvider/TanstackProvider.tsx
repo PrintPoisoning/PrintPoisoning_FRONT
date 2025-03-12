953f535c744d83877fced7ddfc7b6866e142b3d8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-12 14:53:20 +0900

    ✨ Feat :  getQueryClient 리뉴얼

--- src/app/_components/TanstackProvider/TanstackProvider.tsx
+++ src/app/_components/TanstackProvider/TanstackProvider.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 
-import { MINUTE } from "@lib/constants";
+import { getQueryClient } from "@lib/apis";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const TanstackProvider = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: false,
-            refetchOnWindowFocus: false,
-            staleTime: MINUTE,
-            retryOnMount: false,
-          },
-        },
-      }),
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
