a2867b0a9698d0b14b8d56c807f0c2fbf587ca14
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-17 16:04:36 +0900

    ✨ Feat :  Session Provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,27 @@ import "@lib/font/pretendard.css";
 import "@lib/style/globals.css";
 
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 
 import { ToastProvider } from "@lib/components/client";
 import { getStaticMeta } from "@lib/utils";
 
 import { GlobalLayout, TanstackProvider } from "./_components";
+import { RootLayoutProps } from "./layout.type";
 
 export const metadata: Metadata = getStaticMeta();
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, session }: RootLayoutProps) {
   return (
     <html lang="ko-KO">
       <body>
-        <TanstackProvider>
-          <GlobalLayout>
-            <ToastProvider>{children}</ToastProvider>
-          </GlobalLayout>
-        </TanstackProvider>
+        <SessionProvider session={session}>
+          <TanstackProvider>
+            <GlobalLayout>
+              <ToastProvider>{children}</ToastProvider>
+            </GlobalLayout>
+          </TanstackProvider>
+        </SessionProvider>
       </body>
     </html>
   );
