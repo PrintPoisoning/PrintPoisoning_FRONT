30ed689a350512b7051109c4e4f787274cbb1343
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-18 21:16:47 +0900

    🔨 Modify: prettier 규칙 변경

--- src/app/_components/GlobalLayout/GlobalLayout.tsx
+++ src/app/_components/GlobalLayout/GlobalLayout.tsx
@@ -1,11 +1,7 @@
 import { PropsWithChildren } from "react";
 
 const GlobalLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <main className="max-w-layout w-full h-full shadow-layout_shadow">
-      {children}
-    </main>
-  );
+  return <main className="max-w-layout w-full h-full shadow-layout_shadow">{children}</main>;
 };
 
 export default GlobalLayout;
