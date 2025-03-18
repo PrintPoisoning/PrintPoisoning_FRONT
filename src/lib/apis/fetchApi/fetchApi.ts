cd66eb3f715528ae695f74c17972dd4e92f3dd09
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-18 21:16:26 +0900

    ✨ Feat : auth fetch 를 위한 fetchApi 수정

--- src/lib/apis/fetchApi/fetchApi.ts
+++ src/lib/apis/fetchApi/fetchApi.ts
@@ -1,7 +1,18 @@
+import { auth } from "../auth/auth";
+
 const API_KEY = "";
 
-const fetchApi = (url: string, options: RequestInit = {}) => {
-  return fetch(`${API_KEY}${url}`, {
+const fetchApi = async (url: string, options: RequestInit = {}) => {
+  const session = await auth();
+
+  if (!session || !session.sessionToken) {
+    return;
+  }
+
+  // 추후 해당 로직을 통한 토큰 생성
+  console.log("session Token TEST : ", session.sessionToken);
+
+  return await fetch(`${API_KEY}${url}`, {
     ...options,
   });
 };
