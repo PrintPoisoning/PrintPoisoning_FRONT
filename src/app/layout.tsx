c1d6f7dbaf860c51948a6d4eea2fe3805345391d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-19 16:17:25 +0900

    ✨ Feat :  SessionChecker 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { SessionProvider } from "next-auth/react";
 import { ToastProvider } from "@lib/components/client";
 import { getStaticMeta } from "@lib/utils";
 
-import { GlobalLayout, TanstackProvider } from "./_components";
+import { GlobalLayout, SessionChecker, TanstackProvider } from "./_components";
 import { RootLayoutProps } from "./layout.type";
 
 export const metadata: Metadata = getStaticMeta();
@@ -18,9 +18,11 @@ export default function RootLayout({ children, session }: RootLayoutProps) {
       <body>
         <SessionProvider session={session}>
           <TanstackProvider>
-            <GlobalLayout>
-              <ToastProvider>{children}</ToastProvider>
-            </GlobalLayout>
+            <SessionChecker>
+              <GlobalLayout>
+                <ToastProvider>{children}</ToastProvider>
+              </GlobalLayout>
+            </SessionChecker>
           </TanstackProvider>
         </SessionProvider>
       </body>
