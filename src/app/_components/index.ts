db6c9c9086d49fd1c82cbd67f0c1769f21b601ba
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-19 16:17:25 +0900

    ✨ Feat :  SessionChecker 구현

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -1,2 +1,3 @@
 export { default as GlobalLayout } from "./GlobalLayout/GlobalLayout";
 export { default as TanstackProvider } from "./TanstackProvider/TanstackProvider";
+export { default as SessionChecker } from "./SessionChecker/SessionChecker";
