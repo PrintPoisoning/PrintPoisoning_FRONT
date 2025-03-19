7e851cd2522c45412cb5ef35c0cc488d2b4f6e88
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-19 15:44:15 +0900

    ✨ Feat :  LoginPage Assets

---
+++ src/app/login/_components/LoginBackground/_assets/CloudLoginAsset/CloudLoginAsset.tsx
@@ -0,0 +1,95 @@
+const CloudLoginAsset = () => {
+  return (
+    <svg
+      width="147"
+      height="146"
+      viewBox="0 0 147 146"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.6">
+        <g filter="url(#filter0_i_21_424)">
+          <ellipse
+            cx="21.9154"
+            cy="21.4707"
+            rx="21.9154"
+            ry="21.4707"
+            transform="matrix(0.398959 -0.916969 0.920182 0.39149 61.1393 91.2354)"
+            fill="#00C86E"
+          />
+        </g>
+        <ellipse
+          cx="21.4707"
+          cy="21.9154"
+          rx="21.4707"
+          ry="21.9154"
+          transform="matrix(0.920182 0.39149 -0.398959 0.916969 69.0723 27.5244)"
+          fill="#00C86E"
+        />
+        <ellipse
+          cx="21.9154"
+          cy="21.4707"
+          rx="21.9154"
+          ry="21.4707"
+          transform="matrix(0.398959 -0.916969 0.920182 0.39149 28.3037 77.2656)"
+          fill="#00C86E"
+        />
+        <ellipse
+          cx="21.4707"
+          cy="21.9154"
+          rx="21.4707"
+          ry="21.9154"
+          transform="matrix(0.920182 0.39149 -0.398959 0.916969 54.5412 60.923)"
+          fill="#00C86E"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_i_21_424"
+          x="68.029"
+          y="57.7567"
+          width="43.2212"
+          height="53.577"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood
+            floodOpacity="0"
+            result="BackgroundImageFix"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="10" />
+          <feGaussianBlur stdDeviation="5" />
+          <feComposite
+            in2="hardAlpha"
+            operator="arithmetic"
+            k2="-1"
+            k3="1"
+          />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0121913 0 0 0 0 0.5225 0 0 0 0 0.292861 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_21_424"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default CloudLoginAsset;
