da68fadb0417a22b1d7aa90291af3c21bda147d4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-19 15:44:15 +0900

    ✨ Feat :  LoginPage Assets

---
+++ src/app/login/_components/LoginBackground/_assets/index.ts
@@ -0,0 +1,3 @@
+export { default as MascotLoginAsset } from "./MascotLoginAsset/MascotLoginAsset";
+export { default as BookLoginAsset } from "./BookLoginAsset/BookLoginAsset";
+export { default as CloudLoginAsset } from "./CloudLoginAsset/CloudLoginAsset";
