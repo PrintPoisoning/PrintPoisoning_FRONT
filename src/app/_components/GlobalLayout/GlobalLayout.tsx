578863d1f8443d32e7542bdd7676cc9e85b5e76f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-19 15:45:16 +0900

    🎨 Style : GlobalLayout overflow hidden

--- src/app/_components/GlobalLayout/GlobalLayout.tsx
+++ src/app/_components/GlobalLayout/GlobalLayout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 const GlobalLayout = ({ children }: PropsWithChildren) => {
-  return <main className="max-w-layout w-full h-full shadow-layout_shadow">{children}</main>;
+  return <main className="max-w-layout w-full h-full shadow-layout_shadow overflow-hidden">{children}</main>;
 };
 
 export default GlobalLayout;
