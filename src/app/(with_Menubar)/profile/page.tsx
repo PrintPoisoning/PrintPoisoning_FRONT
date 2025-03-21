4b92d9a5a2aeb4cf56a21529a07263f3bf536eb8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-21 21:33:34 +0900

    ✨ Feat : 임시 Profile page

---
+++ src/app/(with_Menubar)/profile/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { AlertModal } from "@lib/containers/client";
+
+const ProfilePage = () => {
+  const router = useRouter();
+
+  return (
+    <section>
+      <AlertModal
+        isShow
+        onClose={() => router.replace("/")}
+        message="현재 준비중인 서비스입니다!"
+      />
+    </section>
+  );
+};
+
+export default ProfilePage;
--- src/lib/containers/client/Menubar/constants/MENU_LIST/MENU_LIST.ts
+++ src/lib/containers/client/Menubar/constants/MENU_LIST/MENU_LIST.ts
@@ -9,7 +9,7 @@ const MENU_LIST = [
   { title: "나의 책풀", href: "/", MenuIcon: MenuCloverIcon },
   { title: "기록", href: "/", MenuIcon: MenuBookIcon },
   { title: "커뮤니티", href: "/", MenuIcon: MenuTalkIcon },
-  { title: "프로필", href: "/", MenuIcon: MenuUserIcon },
+  { title: "프로필", href: "/profile", MenuIcon: MenuUserIcon },
 ] as const;
 
 export default MENU_LIST;
