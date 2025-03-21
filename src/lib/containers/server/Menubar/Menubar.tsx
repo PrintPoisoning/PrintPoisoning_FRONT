bde61162be98c3fa8faa450baa470bceff904779
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-21 16:05:25 +0900

    ✨ Feat :  Menubar 구현

---
+++ src/lib/containers/server/Menubar/Menubar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+
+import React from "react";
+
+import tailwindConfig from "../../../../../tailwind.config";
+import { MENU_LIST } from "./constants";
+import { useActiveMenu } from "./hooks";
+
+import { twMerge } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+
+const Menubar = () => {
+  const { isActiveMenu } = useActiveMenu();
+
+  const { theme } = resolveConfig(tailwindConfig);
+  const { colors } = theme;
+
+  return (
+    <nav className="w-full h-[8rem] flex border-t-[0.1rem] border-main">
+      {MENU_LIST.map(({ title, href, MenuIcon }) => {
+        const isActive = isActiveMenu(href);
+
+        return (
+          <li
+            key={title}
+            className="grow-[1] flex items-center justify-center"
+          >
+            <Link
+              href={href}
+              className={twMerge(
+                "flex flex-col items-center text-[1.2rem] text-gray_400 transition-all",
+                isActive && "text-main",
+              )}
+            >
+              <MenuIcon fill={isActive ? colors.main : colors.gray_400} />
+              {title}
+            </Link>
+          </li>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Menubar;
