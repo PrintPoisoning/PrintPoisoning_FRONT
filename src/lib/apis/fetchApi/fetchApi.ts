88fda10706b0e317a27df2eafc60b2830db8a2c9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-24 14:51:41 +0900

    ✨ Feat :  fetchApi 고도화

--- src/lib/apis/fetchApi/fetchApi.ts
+++ src/lib/apis/fetchApi/fetchApi.ts
@@ -1,20 +1,57 @@
-import { auth } from "../auth/auth";
+import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
-const API_KEY = "";
+// import { auth } from "../auth/auth";
 
-const fetchApi = async (url: string, options: RequestInit = {}) => {
-  const session = await auth();
+const API_KEY = process.env.SERVICE_KEY;
 
-  if (!session || !session.sessionToken) {
-    return;
-  }
+const apiInstance = async (url: string, options: RequestInit = {}) => {
+  // const session = await auth();
+
+  // if (!session || !session.sessionToken) {
+  //   return;
+  // }
 
   // 추후 해당 로직을 통한 토큰 생성
-  console.log("session Token TEST : ", session.sessionToken);
+  // console.log("session Token TEST : ", session.sessionToken);
 
-  return await fetch(`${API_KEY}${url}`, {
+  const res = await fetch(`${API_KEY}${url}`, {
+    ...options,
+  });
+
+  if (!res) {
+    throw new Error(`API Error : ${url}`);
+  }
+
+  if (!res.ok) {
+    const message = JSON.stringify(res.status) ?? "res.ok Error";
+    throw new Error(message);
+  }
+
+  return res;
+};
+
+const post = async <T>(url: string, body: T, options: RequestInit = {}) => {
+  return await apiInstance(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
     ...options,
   });
 };
+
+const get = async <T>(url: string, body: T, options: RequestInit = {}) => {
+  return await apiInstance(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
+
+const fetchApi = { post, get } as const;
 
 export default fetchApi;
