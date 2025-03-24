92028ee5a4e74b02747694943bfc68639f89a54b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-24 22:15:10 +0900

    ✨ Feat :  JWT Type Custom

--- src/lib/types/Auth/next-auth.d.ts
+++ src/lib/types/Auth/next-auth.d.ts
@@ -1,19 +1,32 @@
 import { DefaultSession } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+
+import { IUser } from "../User/User.type";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      test: string;
-    } & DefaultSession["user"];
+    user: IUser & DefaultSession["user"];
 
     sessionToken: string;
     errorMessage: string | null;
   }
 
   interface Account extends DefaultAccount {
-    service?: {
-      accessToken: string | null;
-      refreshToken: string | null;
+    service: {
+      accessToken: string;
+      refreshToken: string;
     };
+  }
+
+  interface JWT extends DefaultJWT {
+    accessToken: string;
+    refreshToken: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken: string;
+    refreshToken: string;
   }
 }
