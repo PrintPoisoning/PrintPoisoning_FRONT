f1f1a3f7505b08266c94e8f46d066d4f17e3991d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-24 20:31:23 +0900

    ✨ Feat :  signup service api

--- src/lib/service/auth/auth.ts
+++ src/lib/service/auth/auth.ts
@@ -1,8 +1,14 @@
 import { fetchApi } from "@lib/apis";
-import { BookFullLoginParams, BookFullLoginResponse } from "@lib/types";
+import { BookFullLoginParams, BookFullLoginResponse, BookFullSignupParams, BookFullSignupResponse } from "@lib/types";
 
 export const bookFullLogin = async (param: BookFullLoginParams) => {
   const res = await fetchApi.post<BookFullLoginParams>("/auth/login", param);
 
   return (await res.json()) as BookFullLoginResponse;
+};
+
+export const bookFullSignup = async (param: BookFullSignupParams) => {
+  const res = await fetchApi.post<BookFullSignupParams>("/users", param);
+
+  return (await res.json()) as BookFullSignupResponse;
 };
