523e221dffa6dfbf58dde47dac755789ea62ffcd
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-24 22:34:19 +0900

    ✨ Feat :  Button Component

--- src/app/(with_Menubar)/(Home)/page.tsx
+++ src/app/(with_Menubar)/(Home)/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@lib/components/client";
+
 const RedingPage = () => {
-  return <main>RedingPage</main>;
+  return (
+    <main>
+      <Button className="bg-red-500">테스트 입니다.</Button>
+    </main>
+  );
 };
 
 export default RedingPage;
--- src/lib/components/client/Button/Button.tsx
+++ src/lib/components/client/Button/Button.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { MouseEvent } from "react";
+import { MouseEvent, useRef } from "react";
 
 import { ButtonProps } from "./Button.type";
+import { Ripple } from "./components";
 
 import { motion } from "motion/react";
 import { twMerge } from "tailwind-merge";
 
 const Button = ({ children, onClick, className, ...rest }: ButtonProps) => {
+  const rippleRef = useRef<{
+    createRipple: (event: MouseEvent<HTMLButtonElement>) => void;
+  }>(null);
+
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const { current } = rippleRef;
+
+    if (current) {
+      current.createRipple(event);
+    }
+
     if (!onClick) return;
     onClick(event);
   };
@@ -20,10 +31,11 @@ const Button = ({ children, onClick, className, ...rest }: ButtonProps) => {
         className,
       )}
       onClick={handleClick}
-      whileTap={{ scale: "0.98" }}
+      whileTap={{ scale: 0.98 }}
       {...rest}
     >
       {children}
+      <Ripple ref={rippleRef} />
     </motion.button>
   );
 };
