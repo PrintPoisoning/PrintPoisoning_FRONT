3ed717b52c496466004dac5da62ea246177d3a86
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-26 22:44:56 +0900

    ✨ Feat :  Kakao LoginButton Button 컴포넌트 적용

--- src/app/login/_components/KakaoLoginButton/KakaoLoginButton.tsx
+++ src/app/login/_components/KakaoLoginButton/KakaoLoginButton.tsx
@@ -1,13 +1,17 @@
 import { singInWithKakao } from "@lib/apis";
+import { Button } from "@lib/components/client";
 import { KakaoLogoIcon } from "@lib/components/server";
 
 const KakaoLoginButton = () => {
   return (
-    <form action={singInWithKakao}>
-      <button className="w-[90%] h-[5.7rem] flex items-center justify-center gap-[1.6rem] rounded-[6rem] absolute bottom-[5.7rem] left-[50%] translate-x-[-50%] bg-kakao_main text-[1.6rem] font-semibold select-none">
+    <form
+      action={singInWithKakao}
+      className="w-full flex justify-center absolute bottom-[5.7rem]"
+    >
+      <Button className="w-[90%] h-[5.7rem] flex items-center justify-center gap-[1.6rem] rounded-[6rem] bg-kakao_main text-[1.6rem] font-semibold select-none">
         <KakaoLogoIcon />
         <p>카카오로 계속하기</p>
-      </button>
+      </Button>
     </form>
   );
 };
