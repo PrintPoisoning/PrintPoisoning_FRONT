07c1b7cdcdf8c4ba30883013df738c25f41343e2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-26 23:10:37 +0900

    ✨ Feat :  Middleware Auth User Redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,29 @@ import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
 import { auth } from "@lib/apis";
 
 export const config: MiddlewareConfig = {
-  matcher: ["/", "/login"],
+  matcher: ["/", "/login", "/community/:path*", "/myBook/:path*", "/profile/:path*", "/record/:path*"],
 };
 
 export const middleware = async (request: NextRequest) => {
   try {
-    console.log("Active Middleware");
-
+    const { pathname } = request.nextUrl;
     const session = await auth();
 
+    const isLoginPage = pathname.startsWith("/login");
+
+    // Login Page
+    const isAuthUser = session && (session.sessionToken || session.user);
+    if (isLoginPage && isAuthUser) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
     console.log("Middleware Session : ", session);
+
+    // Not AuthUser Redirect Home
+    const isNotAuthUser = !session || !session.sessionToken || !session.user;
+    if (!isLoginPage && isNotAuthUser) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
     return NextResponse.next();
   } catch (error) {
