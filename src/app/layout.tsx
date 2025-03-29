a32419f277c645763087f4c4d029a939a466c3c4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-29 21:21:12 +0900

    ➖ Remove:  SessionChecker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { ToastProvider } from "@lib/components/client";
 import { jalnanFont } from "@lib/font/jalnan.font";
 import { getStaticMeta } from "@lib/utils";
 
-import { GlobalLayout, SessionChecker, TanstackProvider } from "./_components";
+import { GlobalLayout, TanstackProvider } from "./_components";
 import { RootLayoutProps } from "./layout.type";
 
 export const metadata: Metadata = getStaticMeta();
@@ -23,11 +23,9 @@ export default function RootLayout({ children, session }: RootLayoutProps) {
       <body>
         <SessionProvider session={session}>
           <TanstackProvider>
-            <SessionChecker>
-              <GlobalLayout>
-                <ToastProvider>{children}</ToastProvider>
-              </GlobalLayout>
-            </SessionChecker>
+            <GlobalLayout>
+              <ToastProvider>{children}</ToastProvider>
+            </GlobalLayout>
           </TanstackProvider>
         </SessionProvider>
       </body>
