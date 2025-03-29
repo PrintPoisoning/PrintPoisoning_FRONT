21837b2f9328f811c98d099d957eff40bddb01e0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-29 21:22:23 +0900

    🔨 Modify:  fetchAPI 주석 제거

--- src/lib/apis/fetchApi/fetchApi.ts
+++ src/lib/apis/fetchApi/fetchApi.ts
@@ -1,19 +1,8 @@
 import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
-// import { auth } from "../auth/auth";
-
 const API_KEY = process.env.SERVICE_KEY;
 
 const apiInstance = async (url: string, options: RequestInit = {}) => {
-  // const session = await auth();
-
-  // if (!session || !session.sessionToken) {
-  //   return;
-  // }
-
-  // 추후 해당 로직을 통한 토큰 생성
-  // console.log("session Token TEST : ", session.sessionToken);
-
   const res = await fetch(`${API_KEY}${url}`, {
     ...options,
   });
