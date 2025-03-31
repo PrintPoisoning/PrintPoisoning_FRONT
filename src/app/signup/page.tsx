96fa165203a3ac4d8b39d228c031e5d52dfa7c67
정화윤 <jeonghwayun0119@gmail.com>
2025-03-31 19:34:58 +0900

    ✨ Feat : Input Component

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+import Input from "@lib/components/client/Input/Input";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Input
+        label="닉네임"
+        className="h-20"
+        placeholder="닉네임"
+      />
+    </div>
+  );
+};
+
+export default SignupPage;
---
+++ src/lib/components/client/Input/Input.type.ts
@@ -0,0 +1,12 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+
+export interface InputProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label?: string;
+  error?: string;
+  placeholder?: string;
+  required?: boolean;
+}
