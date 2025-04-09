e44ca9da9553befed4bed9081cbda96a4a6ddda2
Lee Jae Yeong <leey153@naver.com>
2025-04-09 12:48:41 +0900

    ✨ Feat :  BookFull Error hook 적용

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { Button } from "@lib/components/client";
+import { useBookFullError } from "@lib/hooks";
 
-const globalErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
+const GlobalErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
+  const { message } = useBookFullError({ error });
+
   return (
-    <section className="w-full h-full flex items-center justify-center">
-      <p>에러가 발생했습니다.</p>
-      <p>{error.toString()}</p>
+    <section className="w-full h-full flex flex-col items-center justify-center">
+      <p>{message}</p>
+      <p>잠시 후 다시 시도해주세요.</p>
 
       <Button onClick={reset}>다시 시도</Button>
     </section>
   );
 };
 
-export default globalErrorPage;
+export default GlobalErrorPage;
