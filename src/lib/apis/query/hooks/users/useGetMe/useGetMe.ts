b76a033e93cca7c3725a36c5aa69877c9b235768
Lee Jae Yeong <leey153@naver.com>
2025-04-09 10:56:46 +0900

    🔨 Modify:  useQuery => useSuspenseQuery

--- src/lib/apis/query/hooks/users/useGetMe/useGetMe.ts
+++ src/lib/apis/query/hooks/users/useGetMe/useGetMe.ts
@@ -4,10 +4,10 @@ import { QUERY_KEY } from "@lib/apis/query/constants";
 import { MINUTE } from "@lib/constants";
 import { getMe } from "@lib/service";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 const useGetMe = () => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: QUERY_KEY.USERS.ME(),
     queryFn: getMe,
 
