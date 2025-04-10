508e9461a5c47a2ee9f4748c19cc31b0e6159e20
Lee Jae Yeong <leey153@naver.com>
2025-04-10 15:55:09 +0900

    ✨ Feat :  LoadingBox 최종

--- src/lib/containers/server/LoadingBox/LoadingBox.tsx
+++ src/lib/containers/server/LoadingBox/LoadingBox.tsx
@@ -1,11 +1,15 @@
-import { Skeleton } from "@lib/components/server";
+import { LoadingSpinner, Skeleton } from "@lib/components/server";
 
-const Loading = () => {
+const LoadingBox = () => {
   return (
     <article className="w-full h-full p-[0.6rem] relative flex items-center justify-center">
       <Skeleton className="w-full h-full" />
+
+      <span className="w-[4rem] h-[4rem] absolute">
+        <LoadingSpinner />
+      </span>
     </article>
   );
 };
 
-export default Loading;
+export default LoadingBox;
