683141ea6dbc062d2fbe6d075545cedd948e0166
Lee Jae Yeong <leey153@naver.com>
2025-04-10 16:46:51 +0900

    ♻️ Refactor :  Toast 디자인 적용 및 불 필요 색상 제거

--- src/lib/components/client/Toast/Toast.type.ts
+++ src/lib/components/client/Toast/Toast.type.ts
@@ -1,8 +1,5 @@
-export type ToastType = "alert" | "warning" | "danger" | "safe";
-
 export interface CreateToastOptionsType {
   deleteTime?: number;
-  type?: ToastType;
 }
 
 export type CreateToastFunc = (message: string, options?: CreateToastOptionsType) => void;
@@ -11,5 +8,4 @@ export interface ToastListItemType {
   message: string;
   id: string;
   deleteTime: number;
-  type: ToastType;
 }
