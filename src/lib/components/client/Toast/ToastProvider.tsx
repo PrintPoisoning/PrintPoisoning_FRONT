f0ebc05558c1084bd63b28017b1cbc548b412ed5
Lee Jae Yeong <leey153@naver.com>
2025-04-10 16:46:51 +0900

    ♻️ Refactor :  Toast 디자인 적용 및 불 필요 색상 제거

--- src/lib/components/client/Toast/ToastProvider.tsx
+++ src/lib/components/client/Toast/ToastProvider.tsx
@@ -7,14 +7,14 @@ import { useToastStore } from "./store";
 
 const ToastProvider = ({ children }: PropsWithChildren) => {
   const { toastList, removeToast } = useToastStore();
+
   return (
     <>
       {children}
-      <ul className="absolute top-[1rem] right-[1rem] flex flex-col gap-[0.8rem] text-size14 z-toast">
-        {toastList.map(({ id, message, deleteTime, type }) => (
+      <ul className="absolute bottom-[1rem] right-[50%] translate-x-[50%] flex flex-col gap-[0.8rem] text-size14 z-toast">
+        {toastList.map(({ id, message, deleteTime }) => (
           <ToastItem
             key={id}
-            type={type}
             message={message}
             deleteTime={deleteTime}
             onClose={() => removeToast(id)}
