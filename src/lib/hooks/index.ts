b11290c87f14fa0fd5dd21ae26f663653ee5316b
Lee Jae Yeong <leey153@naver.com>
2025-04-11 14:04:55 +0900

    ✨ Feat :  useWarningFallback hook

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useModal } from "./useModal/useModal";
 export { default as useClickAway } from "./useClickAway/useClickAway";
 export { default as useToast } from "./useToast/useToast";
 export { default as useBookFullError } from "./useBookFullError/useBookFullError";
+export { default as useWarningFallback } from "./useWarningFallback/useWarningFallback";
