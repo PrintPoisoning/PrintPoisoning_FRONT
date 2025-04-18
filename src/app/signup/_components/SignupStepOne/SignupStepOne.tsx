a5b20ce25e4c6a521afbc649cf78ab3b47059be4
Lee Jae Yeong <leey153@naver.com>
2025-04-18 16:28:37 +0900

    ✨ Feat :  SignupStep Component 분리

---
+++ src/app/signup/_components/SignupStepOne/SignupStepOne.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+import { SignupFormValues } from "@app/signup/signup.type";
+
+import { Button, TextInput, ToggleInput } from "@lib/components/client";
+
+const SignupStepOne = () => {
+  const { register, watch, setValue } = useFormContext<SignupFormValues>();
+
+  return (
+    <article className="w-full h-full p-[2rem] flex justify-center items-center flex-col gap-[5rem]">
+      <div className="w-full flex flex-col gap-4">
+        <label className="text-[1.6rem] font-medium text-gray_500 pl-[1rem]">닉네임</label>
+
+        <TextInput
+          {...register("nickname")}
+          className="h-[5rem]"
+        />
+      </div>
+
+      <div className="w-full pl-[1rem] flex flex-col gap-[1rem]">
+        <label className="text-[1.6rem] font-medium text-gray_500">프로필 공개 여부</label>
+
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-1">
+            <p className="text-[1.6rem]">다른 사용자에게 공개하기</p>
+            <p className="text-[1.4rem]">공개 허용 시 모든 사용자가 프로필을 볼 수 있습니다.</p>
+          </div>
+
+          <ToggleInput
+            name="isPublic"
+            checked={!!watch("isPublic")}
+            onChange={e => setValue("isPublic", e.target.checked)}
+          />
+        </div>
+      </div>
+
+      <Button className="bg-main text-white rounded-[6rem] w-full h-[5.7rem] mt-auto mb-[18.5rem] text-[1.6rem] font-semibold">
+        가입하기
+      </Button>
+    </article>
+  );
+};
+
+export default SignupStepOne;
--- src/app/signup/_components/index.ts
+++ src/app/signup/_components/index.ts
@@ -1,2 +1,5 @@
 export { default as SignupHeader } from "./SignupHeader/SignupHeader";
 export { default as SignupForm } from "./SignupForm/SignupForm";
+
+export { default as SignupStepComplete } from "./SignupStepComplete/SignupStepComplete";
+export { default as SignupStepOne } from "./SignupStepOne/SignupStepOne";
