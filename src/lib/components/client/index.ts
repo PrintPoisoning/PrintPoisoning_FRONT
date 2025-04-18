8e1b1a5ab699b3520199a117d6013f1842a71042
Lee Jae Yeong <leey153@naver.com>
2025-04-18 16:28:08 +0900

    ✨ Feat :  Header Import

--- src/lib/components/client/index.ts
+++ src/lib/components/client/index.ts
@@ -1,5 +1,8 @@
 export { default as Modal } from "./Modal/Modal";
 export { default as ToastProvider } from "./Toast/ToastProvider";
 export { default as Button } from "./Button/Button";
+
 export { default as TextInput } from "./Input/TextInput";
 export { default as ToggleInput } from "./Toggle/ToggleInput";
+
+export { default as Header } from "./Header/Header";
