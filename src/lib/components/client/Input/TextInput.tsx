dfd3d60db525471e59fd7cacec969d3e5c119b8d
정화윤 <jeonghwayun0119@gmail.com>
2025-04-21 21:44:27 +0900

    🔨 Modify : forwardfef 대신 리액트 19 버전 ref 사용

--- src/lib/components/client/Input/TextInput.tsx
+++ src/lib/components/client/Input/TextInput.tsx
@@ -1,29 +1,26 @@
 "use client";
 
-import { forwardRef } from "react";
-
 import { InputProps } from "./TextInput.type";
 
 import { twMerge } from "tailwind-merge";
 
-const TextInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ error, className, placeholder, ...rest }, ref) => {
-    return (
-      <div className="w-full flex flex-col gap-[0.8rem]">
-        <input
-          ref={ref}
-          placeholder={placeholder}
-          className={twMerge(
-            "w-full text-[1.6rem] px-8 outline-none bg-[#F8F8F8] text-gray_900 placeholder:text-gray_300 placeholder:text-[1.6rem] rounded-full",
-            className,
-          )}
-          {...rest}
-        />
+const TextInput = ({ ref, error, placeholder, required, className, ...rest }: InputProps) => {
+  return (
+    <div className="w-full flex flex-col gap-[0.8rem]">
+      <input
+        ref={ref}
+        placeholder={placeholder}
+        required={required}
+        className={twMerge(
+          "w-full text-[1.6rem] px-8 outline-none bg-[#F8F8F8] text-gray_900 placeholder:text-gray_300 placeholder:text-[1.6rem] rounded-full",
+          className,
+        )}
+        {...rest}
+      />
 
-        {error && <p className="px-4 text-[1rem] text-toast_danger">{error}</p>}
-      </div>
-    );
-  },
-);
+      {error && <p className="px-4 text-[1rem] text-toast_danger">{error}</p>}
+    </div>
+  );
+};
 
 export default TextInput;
