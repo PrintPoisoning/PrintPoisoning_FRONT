1753502e0ba2704cef9ef31500c3f039921cf6f9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-07-16 21:48:08 +0900

    ✨ Feat :  임시 회원 탈퇴 버튼

--- src/app/(with_Menubar)/(Home)/page.tsx
+++ src/app/(with_Menubar)/(Home)/page.tsx
@@ -1,7 +1,11 @@
-import { signOutWithForm } from "@lib/apis";
+"use client";
+
+import { signOutWithForm, useDeleteUserMutation } from "@lib/apis";
 import { Button } from "@lib/components/client";
 
 const RedingPage = () => {
+  const { mutate: deleteUser } = useDeleteUserMutation();
+
   return (
     <main>
       <Button
@@ -9,6 +13,15 @@ const RedingPage = () => {
         onClick={signOutWithForm}
       >
         임시 로그아웃
+      </Button>
+
+      <Button
+        className="bg-main"
+        onClick={() => {
+          deleteUser();
+        }}
+      >
+        회원탈퇴
       </Button>
     </main>
   );
