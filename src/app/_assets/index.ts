e71d6b73f93cdc608b979c561e38b8c20c35cae4
Lee Jae Yeong <leey153@naver.com>
2025-07-24 08:19:21 +0900

    ✨ Feat :  Confetti Assets 추가

--- src/app/_assets/index.ts
+++ src/app/_assets/index.ts
@@ -3,3 +3,9 @@ export { default as ErrorImage } from "./image/error_logo.png";
 export { default as BookFullMascotImage } from "./image/bf_mascot.png";
 export { default as BookFullCloverImage } from "./image/bf_clover.png";
 export { default as BookFullBookImage } from "./image/bf_book.png";
+
+export { default as BookFullConfetti_1 } from "./image/bf_confetti_1.png";
+export { default as BookFullConfetti_2 } from "./image/bf_confetti_2.png";
+export { default as BookFullConfetti_3 } from "./image/bf_confetti_3.png";
+export { default as BookFullConfetti_4 } from "./image/bf_confetti_4.png";
+export { default as BookFullConfetti_5 } from "./image/bf_confetti_5.png";
